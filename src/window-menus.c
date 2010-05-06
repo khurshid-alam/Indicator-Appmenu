36d99deff409b2430fcaf0c60220bfbe7a7ee373
Ted Gould <ted@gould.cx>
2010-05-07 00:37:56 +0200

    Fleshing out adding an item a little bit

--- src/window-menus.c
+++ src/window-menus.c
@@ -151,6 +151,22 @@ window_menus_get_entries (WindowMenus * wm)
 static void
 menu_entry_added (GtkContainer * container, GtkWidget * widget, gpointer user_data)
 {
+	WindowMenusPrivate * priv = WINDOW_MENUS_GET_PRIVATE(user_data);
+
+	if (!GTK_IS_MENU_ITEM(widget)) {
+		g_warning("Got an item added to the menu which isn't a menu item");
+		return;
+	}
+
+	IndicatorObjectEntry * entry = g_new0(IndicatorObjectEntry, 1);
+
+	/* TODO: Should be a better way for this */
+	entry->label = GTK_LABEL(gtk_label_new(gtk_menu_item_get_label(GTK_MENU_ITEM(widget))));
+	/* TODO: Check for image item */
+	entry->image = NULL;
+	entry->menu = GTK_MENU(gtk_menu_item_get_submenu(GTK_MENU_ITEM(widget)));
+
+	g_array_append_val(priv->entries, entry);
 
 	return;
 }
@@ -159,6 +175,7 @@ menu_entry_added (GtkContainer * container, GtkWidget * widget, gpointer user_da
 static void
 menu_entry_removed (GtkContainer * container, GtkWidget * widget, gpointer user_data)
 {
+	/* TODO */
 
 	return;
 }
