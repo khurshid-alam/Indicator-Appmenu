93d55a5d3d9c8dd6ed15062fdf7587aa90614d0f
Ted Gould <ted@gould.cx>
2010-05-07 10:44:55 +0200

    Setting up the connection and pushing our menus

--- scripts/menu-pusher.c
+++ scripts/menu-pusher.c
@@ -1,7 +1,9 @@
 #include <gtk/gtk.h>
+#include <dbus/dbus-glib.h>
 #include <libdbusmenu-glib/menuitem.h>
 #include <libdbusmenu-glib/server.h>
 #include "../src/dbus-shared.h"
+#include "../src/application-menu-registrar-client.h"
 
 int
 main (int argv, char ** argc)
@@ -43,7 +45,17 @@ main (int argv, char ** argc)
 	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
 
 
+	DbusmenuServer * server = dbusmenu_server_new("/this/is/a/long/object/path");
+	dbusmenu_server_set_root(server, root);
 
+
+	DBusGConnection * session = dbus_g_bus_get(DBUS_BUS_SESSION, NULL);
+	g_return_val_if_fail(session != NULL, 1);
+
+	DBusGProxy * proxy = dbus_g_proxy_new_for_name_owner(session, DBUS_NAME, REG_OBJECT, REG_IFACE, NULL);
+	g_return_val_if_fail(proxy != NULL, 1);
+
+	org_ayatana_indicator_appmenu_registrar_register_window(proxy, 0, "/this/is/a/long/object/path", NULL);
 
 	gtk_main();
 
