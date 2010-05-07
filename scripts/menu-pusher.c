84075b9a2c41f51b239f3647f3806169390860cc
Ted Gould <ted@gould.cx>
2010-05-07 10:35:42 +0200

    Building a menu

--- scripts/menu-pusher.c
+++ scripts/menu-pusher.c
@@ -1,10 +1,46 @@
 #include <gtk/gtk.h>
+#include <libdbusmenu-glib/menuitem.h>
+#include <libdbusmenu-glib/server.h>
+#include "../src/dbus-shared.h"
 
 int
 main (int argv, char ** argc)
 {
 	gtk_init(&argv, &argc);
 
+	DbusmenuMenuitem * root = dbusmenu_menuitem_new();
+
+	DbusmenuMenuitem * firstlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(firstlevel, DBUSMENU_MENUITEM_PROP_LABEL, "File");
+	dbusmenu_menuitem_child_append(root, firstlevel);
+
+	DbusmenuMenuitem * secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Open");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
+
+	secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Save");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
+
+	secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Exit");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
+
+	firstlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(firstlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Edit");
+	dbusmenu_menuitem_child_append(root, firstlevel);
+
+	secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Copy");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
+
+	secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Paste");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
+
+	secondlevel = dbusmenu_menuitem_new();
+	dbusmenu_menuitem_property_set(secondlevel, DBUSMENU_MENUITEM_PROP_LABEL, "Cut");
+	dbusmenu_menuitem_child_append(firstlevel, secondlevel);
 
 
 
