324f0e92f64f46af4fa63493a7ca8e7f1acc5060
Ted Gould <ted@gould.cx>
2010-05-07 16:04:27 +0200

    Running the build, more autogen

--- src/indicator-appmenu-marshal.c
+++ src/indicator-appmenu-marshal.c
@@ -48,20 +48,20 @@
 #endif /* !G_ENABLE_DEBUG */
 
 
-/* VOID:UINT,BOXED (./indicator-appmenu-marshal.list:1) */
+/* VOID:UINT,STRING (./indicator-appmenu-marshal.list:1) */
 void
-_indicator_appmenu_marshal_VOID__UINT_BOXED (GClosure     *closure,
-                                             GValue       *return_value G_GNUC_UNUSED,
-                                             guint         n_param_values,
-                                             const GValue *param_values,
-                                             gpointer      invocation_hint G_GNUC_UNUSED,
-                                             gpointer      marshal_data)
+_indicator_appmenu_marshal_VOID__UINT_STRING (GClosure     *closure,
+                                              GValue       *return_value G_GNUC_UNUSED,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint G_GNUC_UNUSED,
+                                              gpointer      marshal_data)
 {
-  typedef void (*GMarshalFunc_VOID__UINT_BOXED) (gpointer     data1,
-                                                 guint        arg_1,
-                                                 gpointer     arg_2,
-                                                 gpointer     data2);
-  register GMarshalFunc_VOID__UINT_BOXED callback;
+  typedef void (*GMarshalFunc_VOID__UINT_STRING) (gpointer     data1,
+                                                  guint        arg_1,
+                                                  gpointer     arg_2,
+                                                  gpointer     data2);
+  register GMarshalFunc_VOID__UINT_STRING callback;
   register GCClosure *cc = (GCClosure*) closure;
   register gpointer data1, data2;
 
@@ -77,11 +77,11 @@ _indicator_appmenu_marshal_VOID__UINT_BOXED (GClosure     *closure,
       data1 = g_value_peek_pointer (param_values + 0);
       data2 = closure->data;
     }
-  callback = (GMarshalFunc_VOID__UINT_BOXED) (marshal_data ? marshal_data : cc->callback);
+  callback = (GMarshalFunc_VOID__UINT_STRING) (marshal_data ? marshal_data : cc->callback);
 
   callback (data1,
             g_marshal_value_peek_uint (param_values + 1),
-            g_marshal_value_peek_boxed (param_values + 2),
+            g_marshal_value_peek_string (param_values + 2),
             data2);
 }
 
