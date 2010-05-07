58b9e2d9d35214ea846867a1f50893395cd6f8b5
Ted Gould <ted@gould.cx>
2010-05-07 15:59:32 +0200

    Move the menu off of its parent and make sure to show the label.

--- src/window-menus.c
+++ src/window-menus.c
@@ -186,9 +186,17 @@ menu_entry_added (GtkContainer * container, GtkWidget * widget, gpointer user_da
 
 	/* TODO: Should be a better way for this */
 	entry->label = GTK_LABEL(gtk_label_new(gtk_menu_item_get_label(GTK_MENU_ITEM(widget))));
+	gtk_widget_show(GTK_WIDGET(entry->label));
 	/* TODO: Check for image item */
 	entry->image = NULL;
 	entry->menu = GTK_MENU(gtk_menu_item_get_submenu(GTK_MENU_ITEM(widget)));
+
+	if (entry->menu != NULL) {
+		g_object_ref(G_OBJECT(entry->menu));
+		gtk_menu_item_set_submenu(GTK_MENU_ITEM(widget), NULL);
+	} else {
+		g_warning("No menu!");
+	}
 
 	g_array_append_val(priv->entries, entry);
 	return;
