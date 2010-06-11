4410ea8860400e9e2bcdee8d5f0e54bf0cfe4538
Ted Gould <ted@gould.cx>
2010-06-11 15:28:33 -0500

    * debian/indicator-appmenu-crashdb.conf: Change package name to not   be a literal. * debian/source_indicator-appmenu.py: Fix array accessor.

--- debian/source_indicator-appmenu.py
+++ debian/source_indicator-appmenu.py
@@ -1,6 +1,6 @@
 import os.path
 
 def add_info(report):
-	if not apport.packaging.is_distro_package(report['indicator-appmenu'].split(0)):
+	if not apport.packaging.is_distro_package(report['Package'].split()[0]):
 		report['ThirdParty'] = 'True'
-		report['CrashDB'] = 'indicator-appmenu'
+		report['CrashDB'] = 'indicator_appmenu'
