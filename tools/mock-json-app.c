d577988c9f3cbb37043abd241f648db00bd95597
Ted Gould <ted@gould.cx>
2010-06-29 11:02:12 -0500

    Quit when window gets destroyed

--- tools/mock-json-app.c
+++ tools/mock-json-app.c
@@ -1,12 +1,22 @@
 
 #include <gtk/gtk.h>
 
+GtkWidget * window = NULL;
+
+static void
+destroy_cb (void)
+{
+	gtk_main_quit();
+	return;
+}
+
 int
 main (int argv, char ** argc)
 {
 	gtk_init(&argv, &argc);
 
-	GtkWidget * window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy_cb), NULL);
 	gtk_widget_show(window);
 
 	gtk_main();
