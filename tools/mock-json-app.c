59071e4fb6fe5212f1902c545d1781310af2d33a
Ted Gould <ted@gould.cx>
2010-06-29 11:21:57 -0500

    Building our menus -- woot!

--- tools/mock-json-app.c
+++ tools/mock-json-app.c
@@ -1,11 +1,21 @@
 
 #include <gtk/gtk.h>
+#include <libdbusmenu-glib/server.h>
+#include <libdbusmenu-jsonloader/json-loader.h>
+
+#define MENU_PATH "/mock/json/app/menu"
 
 GtkWidget * window = NULL;
+DbusmenuServer * server = NULL;
 
 static gboolean
 idle_func (gpointer user_data)
 {
+	DbusmenuMenuitem * root = dbusmenu_json_build_from_file((const gchar *)user_data);
+	g_return_val_if_fail(root != NULL, FALSE);
+
+	DbusmenuServer * server = dbusmenu_server_new(MENU_PATH);
+	dbusmenu_server_set_root(server, root);
 
 	return FALSE;
 }
@@ -26,7 +36,7 @@ main (int argv, char ** argc)
 	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy_cb), NULL);
 	gtk_widget_show(window);
 
-	g_idle_add(idle_func, NULL);
+	g_idle_add(idle_func, argc[1]);
 
 	gtk_main();
 
