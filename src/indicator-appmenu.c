bdf83e6e4749b1eb0c5de96c2938acc804162ebf
Ted Gould <ted@gould.cx>
2010-09-03 11:30:09 -0500

    Getting a close function together.

--- src/indicator-appmenu.c
+++ src/indicator-appmenu.c
@@ -23,6 +23,9 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config.h"
 #endif
 
+#include <X11/Xlib.h>
+#include <gdk/gdkx.h>
+
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-bindings.h>
 #include <dbus/dbus-glib-lowlevel.h>
@@ -336,6 +339,46 @@ indicator_appmenu_debug_init (IndicatorAppmenuDebug *self)
 	return;
 }
 
+/* Close the current application using magic */
+static void
+close_current (GtkMenuItem * mi, gpointer user_data)
+{
+	IndicatorAppmenu * iapp = INDICATOR_APPMENU(user_data);
+
+	if (iapp->active_window == NULL) {
+		g_warning("Can't close a window we don't have.  NULL not cool.");
+		return;
+	}
+
+	guint xid = bamf_window_get_xid(iapp->active_window);
+	guint timestamp = gdk_event_get_time(NULL);
+
+	XEvent xev;
+
+	xev.xclient.type = ClientMessage;
+	xev.xclient.serial = 0;
+	xev.xclient.send_event = True;
+	xev.xclient.display = gdk_x11_get_default_xdisplay ();
+	xev.xclient.window = xid;
+	xev.xclient.message_type = gdk_x11_atom_to_xatom (gdk_atom_intern ("_NET_CLOSE_WINDOW", TRUE));
+	xev.xclient.format = 32;
+	xev.xclient.data.l[0] = timestamp;
+	xev.xclient.data.l[1] = 2; /* Client type pager, so it listens to us */
+	xev.xclient.data.l[2] = 0;
+	xev.xclient.data.l[3] = 0;
+	xev.xclient.data.l[4] = 0;
+
+	gdk_error_trap_push ();
+	XSendEvent (gdk_x11_get_default_xdisplay (),
+	            gdk_x11_get_default_root_xwindow (),
+	            False,
+	            SubstructureRedirectMask | SubstructureNotifyMask,
+	            &xev);
+	gdk_error_trap_pop ();
+
+	return;
+}
+
 /* Create the default window menus */
 static void
 build_window_menus (IndicatorAppmenu * iapp)
@@ -359,6 +402,7 @@ build_window_menus (IndicatorAppmenu * iapp)
 
 	mi = GTK_MENU_ITEM(gtk_image_menu_item_new_from_stock(GTK_STOCK_CLOSE, agroup));
 	gtk_widget_set_sensitive(GTK_WIDGET(mi), FALSE);
+	g_signal_connect(G_OBJECT(mi), "activate", G_CALLBACK(close_current), iapp);
 	gtk_widget_show(GTK_WIDGET(mi));
 	gtk_menu_append(entries[0].menu, GTK_WIDGET(mi));
 	iapp->close_item = mi;
