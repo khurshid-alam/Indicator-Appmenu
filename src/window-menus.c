1f55b14adbb9e9b557d0a622850d70001fab8cc2
Ted Gould <ted@gould.cx>
2011-02-17 14:01:38 -0600

    Emitting the a11y update signal when the label updates

--- src/window-menus.c
+++ src/window-menus.c
@@ -620,7 +620,10 @@ menu_prop_changed (DbusmenuMenuitem * item, const gchar * property, GVariant * v
 	} else if (!g_strcmp0(property, DBUSMENU_MENUITEM_PROP_LABEL)) {
 		gtk_label_set_text_with_mnemonic(entry->label, g_variant_get_string(value, NULL));
 		entry->accessible_desc = g_variant_get_string(value, NULL);
-		/* FIXME: Need to signal a change in the accessible description. */
+
+		if (wmentry->wm != NULL) {
+			g_signal_emit(G_OBJECT(wmentry->wm), A11Y_UPDATE, 0, entry, TRUE);
+		}
 	}
 
 	return;
