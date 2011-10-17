2bb1976e891357f538e950b4d66064d36bad63a3
Ted Gould <ted@gould.cx>
2011-10-17 16:24:17 -0500

    Basic little getting of std in and a little main loop love

--- service/hud-cli.c
+++ service/hud-cli.c
@@ -1,9 +1,23 @@
 
+#include <glib.h>
+#include <gio/gunixinputstream.h>
+#include <unistd.h>
+
+GMainLoop * mainloop = NULL;
+
 int
 main (int argv, char * argc[])
 {
+	g_type_init();
 
+	GInputStream * stdinput = g_unix_input_stream_new(STDIN_FILENO, FALSE);
+	g_return_val_if_fail(stdinput != NULL, 1);
 
+	mainloop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(mainloop);
+
+	g_object_unref(mainloop);
+	g_object_unref(stdinput);
 
 	return 0;
 }
