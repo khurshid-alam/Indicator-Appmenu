c1bf8d2feb22dfa9eeb5868350c9a20914f3ed71
Ted Gould <ted@gould.cx>
2011-10-17 22:17:52 -0500

    Getting the name in the service

--- service/hud-service.c
+++ service/hud-service.c
@@ -1,7 +1,19 @@
 
 #include <glib.h>
+#include <gio/gio.h>
 
+#include "shared-values.h"
 #include "hud-dbus.h"
+
+static GMainLoop * mainloop = NULL;
+
+static void
+name_lost_cb (GDBusConnection * connection, const gchar * name, gpointer user_data)
+{
+	g_error("Unable to get name '%s'", name);
+	g_main_loop_quit(mainloop);
+	return;
+}
 
 int
 main (int argv, char * argc[])
@@ -10,7 +22,17 @@ main (int argv, char * argc[])
 
 	HudDbus * dbus = hud_dbus_new();
 
-	GMainLoop * mainloop = g_main_loop_new(NULL, FALSE);
+	g_bus_own_name(G_BUS_TYPE_SESSION,
+	/* Name */           DBUS_NAME,
+	/* Flags */          G_BUS_NAME_OWNER_FLAGS_NONE,
+	/* Bus Acquired */   NULL,
+	/* Name Acquired */  NULL,
+	/* Name Lost */      name_lost_cb,
+	/* User Data */      NULL,
+	/* Destroy */        NULL);
+
+
+	mainloop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(mainloop);
 
 	g_main_loop_unref(mainloop);
