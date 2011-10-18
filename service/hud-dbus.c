9af1a3229ff7678346498922b1de73e88213b31e
Ted Gould <ted@gould.cx>
2011-10-17 22:11:12 -0500

    Fleshing out the methods

--- service/hud-dbus.c
+++ service/hud-dbus.c
@@ -31,6 +31,9 @@ static void bus_method          (GDBusConnection *connection,
                                  GVariant *parameters,
                                  GDBusMethodInvocation *invocation,
                                  gpointer user_data);
+static GVariant * get_suggestions (const gchar * query);
+static void execute_query (const gchar * query);
+
 
 G_DEFINE_TYPE (HudDbus, hud_dbus, G_TYPE_OBJECT);
 static GDBusNodeInfo * node_info = NULL;
@@ -157,7 +160,56 @@ bus_got_cb (GObject *object, GAsyncResult * res, gpointer user_data)
 static void
 bus_method (GDBusConnection *connection, const gchar *sender, const gchar *object_path, const gchar *interface_name, const gchar *method_name, GVariant *parameters, GDBusMethodInvocation *invocation, gpointer user_data)
 {
+	if (g_strcmp0(method_name, "GetSuggestions") == 0) {
+		GVariant * ret = NULL;
+		gchar * query = NULL;
 
+		g_variant_get(parameters, "(s)", &query);
+
+		ret = get_suggestions(query);
+
+		g_dbus_method_invocation_return_value(invocation, ret);
+		g_free(query);
+	} else if (g_strcmp0(method_name, "ExecuteQuery") == 0) {
+		gchar * query = NULL;
+
+		g_variant_get(parameters, "(s)", &query);
+
+		execute_query(query);
+		
+		g_dbus_method_invocation_return_value(invocation, NULL);
+		g_free(query);
+	}
+
+	return;
+}
+
+static GVariant *
+get_suggestions (const gchar * query)
+{
+	GVariantBuilder ret;
+	GVariantBuilder suggests;
+
+	g_variant_builder_init(&ret, G_VARIANT_TYPE_TUPLE);
+
+	g_variant_builder_add_value(&ret, g_variant_new_string("New Document"));
+
+	g_variant_builder_init(&suggests, G_VARIANT_TYPE_ARRAY);
+
+	g_variant_builder_add_value(&suggests, g_variant_new_string("Suggestion 1"));
+	g_variant_builder_add_value(&suggests, g_variant_new_string("Suggestion 2"));
+	g_variant_builder_add_value(&suggests, g_variant_new_string("Suggestion 3"));
+	g_variant_builder_add_value(&suggests, g_variant_new_string("Suggestion 4"));
+	g_variant_builder_add_value(&suggests, g_variant_new_string("Suggestion 5"));
+
+	g_variant_builder_add_value(&ret, g_variant_builder_end(&suggests));
+	return g_variant_builder_end(&ret);
+}
+
+static void
+execute_query (const gchar * query)
+{
+	g_debug("Execute: %s", query);
 
 	return;
 }
