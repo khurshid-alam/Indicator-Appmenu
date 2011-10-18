d9f77f97fa87f11cc1c5a4c126c2ca5d7b298608
Ted Gould <ted@gould.cx>
2011-10-17 20:58:34 -0500

    Filling out the 'HUD Service' -- we better get these other objects to do some work!

--- service/hud-service.c
+++ service/hud-service.c
@@ -1,8 +1,20 @@
 
+#include <glib.h>
+
+#include "hud-dbus.h"
+
 int
 main (int argv, char * argc[])
 {
+	g_type_init();
 
+	HudDbus * dbus = hud_dbus_new();
+
+	GMainLoop * mainloop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(mainloop);
+
+	g_main_loop_unref(mainloop);
+	g_object_unref(dbus);
 
 	return 0;
 }
