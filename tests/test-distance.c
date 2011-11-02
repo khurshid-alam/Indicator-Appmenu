88d8ed1848297489de4f28a71efb3a2320eeb07d
Ted Gould <ted@gould.cx>
2011-11-02 12:34:27 -0400

    Adding a test to find the best string -- it fails

--- tests/test-distance.c
+++ tests/test-distance.c
@@ -4,14 +4,41 @@
 #include "../service/distance.h"
 #include "../service/distance.c"
 
-/* Ensure the parser can find children */
+/* Ensure the base calculation works */
 static void
-test_distance_base (void) {
-
+test_distance_base (void)
+{
 	g_assert(calculate_distance("foo", "foo") == 0);
 	g_assert(calculate_distance("foo", "bar") != 0);
 	g_assert(calculate_distance("foo", NULL) != 0);
 	g_assert(calculate_distance(NULL, "foo") != 0);
+
+	return;
+}
+
+/* Ensure the base calculation works */
+static void
+test_distance_subfunction (void)
+{
+	const gchar * teststrings[] = {
+		"File > Open",
+		"File > New",
+		"File > Print",
+		"File > Print Preview",
+	};
+	const gchar * search = "Print Pre";
+	int i;
+	int right = 3;
+	int num_tests = 4;
+
+	for (i = 0; i < num_tests; i++) {
+		if (i == right)
+			continue;
+
+		if (calculate_distance(search, teststrings[i]) < calculate_distance(search, teststrings[right])) {
+			g_error("Found '%s' with search string '%s' instead of '%s'", teststrings[i], search, teststrings[right]);
+		}
+	}
 
 	return;
 }
@@ -21,6 +48,7 @@ static void
 test_distance_suite (void)
 {
 	g_test_add_func ("/hud/distance/base",          test_distance_base);
+	g_test_add_func ("/hud/distance/subfunction",   test_distance_subfunction);
 	return;
 }
 
