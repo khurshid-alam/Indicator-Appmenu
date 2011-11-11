3b103e8f25c0b1da7c55060260211261b8095853
Ted Gould <ted@gould.cx>
2011-11-11 10:08:10 -0600

    Adding an insert test to make sure we can record the entires as well

--- tests/test-usage-db-simple.c
+++ tests/test-usage-db-simple.c
@@ -33,12 +33,38 @@ test_usage_db_counts (void)
 	return;
 }
 
+static void
+test_usage_db_insert (void)
+{
+	UsageTracker * tracker = usage_tracker_new();
+	g_assert(tracker != NULL);
+	g_assert(IS_USAGE_TRACKER(tracker));
+
+	int i = 0;
+
+	for (i = 0; i < 5; i++) {
+		usage_tracker_mark_usage(tracker, "testapp.desktop", "Five");
+	}
+
+	g_assert(usage_tracker_get_usage(tracker, "testapp.desktop", "Five") == 5);
+
+	for (i = 0; i < 100; i++) {
+		usage_tracker_mark_usage(tracker, "testapp.desktop", "Hundred");
+	}
+
+	g_assert(usage_tracker_get_usage(tracker, "testapp.desktop", "Hundred") == 100);
+
+	g_object_unref(tracker);
+	return;
+}
+
 /* Build the test suite */
 static void
 test_usage_db_suite (void)
 {
 	g_test_add_func ("/hud/usage/simple/base",          test_usage_db_base);
 	g_test_add_func ("/hud/usage/simple/counts",        test_usage_db_counts);
+	g_test_add_func ("/hud/usage/simple/insert",        test_usage_db_insert);
 	return;
 }
 
