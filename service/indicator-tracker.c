03d645d278846e51fa9fa40704634172674d9447
Ted Gould <ted@gould.cx>
2011-11-11 21:09:26 -0600

    Adding an object for tracking the indicators

---
+++ service/indicator-tracker.c
@@ -0,0 +1,73 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "indicator-tracker.h"
+
+struct _IndicatorTrackerPrivate {
+	GArray * indicators;
+};
+
+#define INDICATOR_TRACKER_GET_PRIVATE(o) \
+(G_TYPE_INSTANCE_GET_PRIVATE ((o), INDICATOR_TRACKER_TYPE, IndicatorTrackerPrivate))
+
+static void indicator_tracker_class_init (IndicatorTrackerClass *klass);
+static void indicator_tracker_init       (IndicatorTracker *self);
+static void indicator_tracker_dispose    (GObject *object);
+static void indicator_tracker_finalize   (GObject *object);
+
+G_DEFINE_TYPE (IndicatorTracker, indicator_tracker, G_TYPE_OBJECT);
+
+static void
+indicator_tracker_class_init (IndicatorTrackerClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (klass, sizeof (IndicatorTrackerPrivate));
+
+	object_class->dispose = indicator_tracker_dispose;
+	object_class->finalize = indicator_tracker_finalize;
+
+	return;
+}
+
+static void
+indicator_tracker_init (IndicatorTracker *self)
+{
+	self->priv = INDICATOR_TRACKER_GET_PRIVATE(self);
+	self->priv->indicators = NULL;
+
+	self->priv->indicators = g_array_new(FALSE, TRUE, sizeof(IndicatorTrackerIndicator));
+
+
+	return;
+}
+
+static void
+indicator_tracker_dispose (GObject *object)
+{
+
+	G_OBJECT_CLASS (indicator_tracker_parent_class)->dispose (object);
+	return;
+}
+
+static void
+indicator_tracker_finalize (GObject *object)
+{
+
+	G_OBJECT_CLASS (indicator_tracker_parent_class)->finalize (object);
+	return;
+}
+
+IndicatorTracker *
+indicator_tracker_new (void)
+{
+	return INDICATOR_TRACKER(g_object_new(INDICATOR_TRACKER_TYPE, NULL));
+}
+
+GArray *
+indicator_tracker_get_indicators (IndicatorTracker * tracker)
+{
+	g_return_val_if_fail(IS_INDICATOR_TRACKER(tracker), NULL);
+	return tracker->priv->indicators;
+}
---
+++ service/indicator-tracker.h
@@ -0,0 +1,42 @@
+#ifndef __INDICATOR_TRACKER_H__
+#define __INDICATOR_TRACKER_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define INDICATOR_TRACKER_TYPE            (indicator_tracker_get_type ())
+#define INDICATOR_TRACKER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), INDICATOR_TRACKER_TYPE, IndicatorTracker))
+#define INDICATOR_TRACKER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), INDICATOR_TRACKER_TYPE, IndicatorTrackerClass))
+#define IS_INDICATOR_TRACKER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), INDICATOR_TRACKER_TYPE))
+#define IS_INDICATOR_TRACKER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), INDICATOR_TRACKER_TYPE))
+#define INDICATOR_TRACKER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), INDICATOR_TRACKER_TYPE, IndicatorTrackerClass))
+
+typedef struct _IndicatorTracker          IndicatorTracker;
+typedef struct _IndicatorTrackerClass     IndicatorTrackerClass;
+typedef struct _IndicatorTrackerPrivate   IndicatorTrackerPrivate;
+typedef struct _IndicatorTrackerIndicator IndicatorTrackerIndicator;
+
+struct _IndicatorTrackerClass {
+	GObjectClass parent_class;
+};
+
+struct _IndicatorTracker {
+	GObject parent;
+	IndicatorTrackerPrivate * priv;
+};
+
+struct _IndicatorTrackerIndicator {
+	gchar * name;
+	gchar * dbus_name;
+	gchar * dbus_object;
+};
+
+GType indicator_tracker_get_type (void);
+IndicatorTracker * indicator_tracker_new (void);
+GArray * indicator_tracker_get_inidcators (IndicatorTracker * tracker);
+
+G_END_DECLS
+
+#endif
