0e0059d41d9623c7548a2da751c26ba9ea3fc063
Ted Gould <ted@gould.cx>
2011-11-23 21:08:55 -0600

    Create the utility verify the app info files

---
+++ service/hud-verify-app-info.c
@@ -0,0 +1,77 @@
+/*
+Verify the validity of the App Info file to make sure it parses
+
+Copyright 2011 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License version 3, as published 
+by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranties of 
+MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <unistd.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include "load-app-info.h"
+
+int
+main (int argv, char * argc[])
+{
+	gboolean passed = TRUE;
+
+	if (argv != 2) {
+		g_printerr("Usage: %s <app-info file path>\n", argc[0]);
+		return 1;
+	}
+
+	gchar * filename = NULL;
+	gint tmpfile = g_file_open_tmp("hud-verify-app-info-temp-db-XXXXXX", &filename, NULL);
+
+	if (tmpfile < 0) {
+		passed = FALSE;
+		goto cleanup;
+	}
+
+	close(tmpfile);
+	/* NOTE: there is a small security bug here in that we're closing the
+	   file and reopening it, so the temp isn't really gauranteed to be
+	   safe.  But, I don't think we're really worried about security in this
+	   utility. */
+
+	sqlite3 * db = NULL;
+	int open_status = sqlite3_open(filename, &db); 
+
+	if (open_status != SQLITE_OK) {
+		g_warning("Error opening usage DB: %s", filename);
+		passed = FALSE;
+		goto cleanup;
+	}
+
+	passed = load_app_info(argc[1], db);
+
+cleanup:
+	if (db != NULL) {
+		sqlite3_close(db);
+	}
+
+	if (filename != NULL) {
+		g_unlink(filename);
+		g_free(filename);
+	}
+
+	if (passed) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
