a86d098fb6bc6953bf5f698debff3e74aab0a090
Ted Gould <ted@gould.cx>
2012-03-06 11:04:40 -0600

    API interfacing to the class

--- src/window-menu.c
+++ src/window-menu.c
@@ -47,3 +47,115 @@ window_menu_finalize (GObject *object)
 	G_OBJECT_CLASS (window_menu_parent_class)->finalize (object);
 	return;
 }
+
+
+/**************************
+  API
+ **************************/
+GList *
+window_menu_get_entries (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_entries != NULL) {
+		return class->get_entries(wm);
+	} else {
+		return NULL;
+	}
+}
+
+guint
+window_menu_get_location (WindowMenu * wm, IndicatorObjectEntry * entry)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_location != NULL) {
+		return class->get_location(wm, entry);
+	} else {
+		return 0;
+	}
+}
+
+guint
+window_menu_get_xid (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_xid != NULL) {
+		return class->get_xid(wm);
+	} else {
+		return 0;
+	}
+}
+
+gchar *
+window_menu_get_path (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_path != NULL) {
+		return class->get_path(wm);
+	} else {
+		return NULL;
+	}
+}
+
+gchar *
+window_menu_get_address (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_address != NULL) {
+		return class->get_address(wm);
+	} else {
+		return NULL;
+	}
+}
+
+gboolean
+window_menu_get_error_state (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_error_state != NULL) {
+		return class->get_error_state(wm);
+	} else {
+		return TRUE;
+	}
+}
+
+WindowMenuStatus
+window_menu_get_status (WindowMenu * wm)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->get_status != NULL) {
+		return class->get_status(wm);
+	} else {
+		return WINDOW_MENU_STATUS_NORMAL;
+	}
+}
+
+void
+window_menu_entry_restore (WindowMenu * wm, IndicatorObjectEntry * entry)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->entry_restore != NULL) {
+		return class->entry_restore(wm, entry);
+	} else {
+		return;
+	}
+}
+
+void
+window_menu_entry_activate (WindowMenu * wm, IndicatorObjectEntry * entry, guint timestamp)
+{
+	WindowMenuClass * class = WINDOW_MENU_GET_CLASS(wm);
+
+	if (class->entry_activate != NULL) {
+		return class->entry_activate(wm, entry, timestamp);
+	} else {
+		return;
+	}
+}
