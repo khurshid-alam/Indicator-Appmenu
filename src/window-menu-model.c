d036b1eb43a5ebce05511137b97afe0d9bb351cb
Ted Gould <ted@gould.cx>
2012-03-21 09:05:28 -0500

    Oops, forgot copyright headers.

--- src/window-menu-model.c
+++ src/window-menu-model.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2012 Canonical Limited
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Ted Gould <ted.gould@canonical.com>
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
